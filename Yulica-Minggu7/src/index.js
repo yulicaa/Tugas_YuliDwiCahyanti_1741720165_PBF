08e3e15b0e8bbf457f8cd164a000b176ab51959e
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-03-18 12:58:09 +0700

    Praktikum minggu 7

---
+++ Yulica-Minggu7/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+      {" "}
+      <App />{" "}
+    </Provider>,
+    document.getElementById("root")
+  );
+  serviceWorker.register();
