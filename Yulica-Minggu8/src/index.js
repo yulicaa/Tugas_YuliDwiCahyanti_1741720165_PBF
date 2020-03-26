b2de96cf4a6b1d774ba2bcd08a7f5c2010b8faba
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-03-26 16:58:08 +0700

    js8 contact-redux

---
+++ Yulica-Minggu8/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+const store = createStore(rootReducer);
+  ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+
+serviceWorker.register();
