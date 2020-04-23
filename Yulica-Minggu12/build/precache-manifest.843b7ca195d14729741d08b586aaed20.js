d10cafbc91196b574eddb2d91741bdc372e27a52
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-04-23 11:44:08 +0700

    Tugas 12 fixx

---
+++ Yulica-Minggu12/build/precache-manifest.843b7ca195d14729741d08b586aaed20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55685914136ceecd80197da62c1908b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8b67ec99a84ce8f7623",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "980680c161ebb54aa6aa",
+    "url": "/static/js/2.84ff9ba8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84ff9ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b67ec99a84ce8f7623",
+    "url": "/static/js/main.ba057f24.chunk.js"
+  },
+  {
+    "revision": "4d6660af49a15d482a91",
+    "url": "/static/js/runtime-main.136dee2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
