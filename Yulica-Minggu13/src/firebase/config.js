8a8f48a55a5770bea5f3ba6d30ca6cb4e178f457
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-04-30 22:43:19 +0700

    Tugas Minggu13

---
+++ Yulica-Minggu13/src/firebase/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1kqx-0fNp5nlYaLmrQm7gAp6N8OS-p48",
+    authDomain: "yulica-firebase-minggu12.firebaseapp.com",
+    databaseURL: "https://yulica-firebase-minggu12.firebaseio.com",
+    projectId: "yulica-firebase-minggu12",
+    storageBucket: "yulica-firebase-minggu12.appspot.com",
+    messagingSenderId: "990300465456",
+    appId: "1:990300465456:web:a466cb3e97a071ca3540f5",
+    measurementId: "G-S48QRN3605"
+};
+
+export default firebaseConfig; 
+
